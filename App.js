039d1ab5929a77791b0b184e744394beb24fc503
Muhammad Ali Gulzar <ali@Muhammads-MBP-2.home>
2023-04-18 02:13:54 +0300

    Add bottom navigation bar

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Provider as PaperProvider, BottomNavigation, Text } from 'react-native-paper'
+
+const SearchRoute = () => <Text>Search</Text>
+
+const BrowseRoute = () => <Text>Browse</Text>
+
+const ProfileRoute = () => <Text>Profile</Text>
+
+const renderScene = BottomNavigation.SceneMap({
+  search: SearchRoute,
+  browse: BrowseRoute,
+  profile: ProfileRoute
+})
 
 export default function App() {
+  const [index, setIndex] = useState(0)
+  const [routes] = useState([
+    {
+      key: 'search',
+      title: 'Search',
+      focusedIcon: 'clipboard-search',
+      unfocusedIcon: 'clipboard-search-outline'
+    },
+    { key: 'browse', title: 'Browse', focusedIcon: 'archive', unfocusedIcon: 'archive-outline' },
+    {
+      key: 'profile',
+      title: 'Profile',
+      focusedIcon: 'face-man-shimmer',
+      unfocusedIcon: 'face-man-shimmer-outline'
+    }
+  ])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <PaperProvider>
+      <View style={styles.container}>
+        <BottomNavigation
+          navigationState={{ index, routes }}
+          onIndexChange={setIndex}
+          renderScene={renderScene}
+        />
+      </View>
+    </PaperProvider>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    flex: 1
+  }
+})
